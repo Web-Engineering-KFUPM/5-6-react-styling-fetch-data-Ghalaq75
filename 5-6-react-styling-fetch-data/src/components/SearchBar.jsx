70aa4200da552e1e88fbc88d7f416be68d8095f9
Ghalaq75 <ghalaq75@gmail.com>
2026-03-04 12:56:09 +0300

    TODO 1

--- 5-6-react-styling-fetch-data/src/components/SearchBar.jsx
+++ 5-6-react-styling-fetch-data/src/components/SearchBar.jsx
@@ -4,8 +4,8 @@ import { Form, InputGroup } from "react-bootstrap";
 function SearchBar({ searchTerm, onSearchChange }) {
   return (
     // TODO 1.2: Set wrapper div className EXACTLY to "mb-4"
-    <div className="">
-      <InputGroup>
+<div className="mb-4">
+        <InputGroup>
         <InputGroup.Text>Search</InputGroup.Text>
         <Form.Control
           type="text"
